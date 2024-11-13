0ab233a9130f4c89e0bbd0f62113bb03dc457079
azhar-mansoori <azharuddin.10121995@gmail.com>
2024-11-14 04:12:22 +0530

    final commit

---
+++ ui/js/custom/common.js
@@ -0,0 +1,63 @@
+// Define your api here
+var productListApiUrl = 'http://127.0.0.1:5000/getProducts';  //getting products from database to display
+var uomListApiUrl = 'http://127.0.0.1:5000/getUOM';              //getting uom names from database to show in as options[each,kg]
+var productSaveApiUrl = 'http://127.0.0.1:5000/insertProduct';   //to save the inserted products into database 
+var productDeleteApiUrl = 'http://127.0.0.1:5000/deleteProduct';  //to delete a product
+var orderListApiUrl = 'http://127.0.0.1:5000/getAllOrders';
+var orderSaveApiUrl = 'http://127.0.0.1:5000/insertOrder';
+
+// For product drop in order
+var productsApiUrl = 'https://fakestoreapi.com/products';
+
+function callApi(method, url, data) {
+    $.ajax({
+        method: method,
+        url: url,
+        data: data
+    }).done(function( msg ) {
+        window.location.reload();
+    });
+}
+
+function calculateValue() {
+    var total = 0;
+    $(".product-item").each(function( index ) {
+        var qty = parseFloat($(this).find('.product-qty').val());
+        var price = parseFloat($(this).find('#product_price').val());
+        price = price*qty;
+        $(this).find('#item_total').val(price.toFixed(2));
+        total += price;
+    });
+    $("#product_grand_total").val(total.toFixed(2));
+}
+
+function orderParser(order) {
+    return {
+        id : order.id,
+        date : order.employee_name,
+        orderNo : order.employee_name,
+        customerName : order.employee_name,
+        cost : parseInt(order.employee_salary)
+    }
+}
+
+function productParser(product) {
+    return {
+        id : product.id,
+        name : product.employee_name,
+        unit : product.employee_name,
+        price : product.employee_name
+    }
+}
+
+function productDropParser(product) {
+    return {
+        id : product.id,
+        name : product.title
+    }
+}
+
+//To enable bootstrap tooltip globally
+// $(function () {
+//     $('[data-toggle="tooltip"]').tooltip()
+// });
